4f9e043ca1dd1e40003af15fbf0b3bef664b9503
Bianca Magalhaes <biamagalhaes.tur@gmail.com>
2022-01-12 20:07:13 -0300

    Aula 01 - jan12
    Seja o que Deus quiser

---
+++ my_initial.rb
@@ -0,0 +1,20 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+# SINTAXE ------------------------------------------------------
+# "um comentário é uma linha que o programa ignora"
+# "ele serve para deixar mensagens ocultas para outros programadores"
+# ele começa com hashtag
+# puts "E aí cambada"
+puts "já chegou o disco voador"
+puts "let's eat!!!"
+puts "bianca" + "magalhaes"
+# -----------------------------------------------------------------------
+
+puts "32" + "9875"
+puts 32+38
+puts 2.7+0.3
+puts 6.3/2.7
+
+# ----------------------------------------
+
+
---
+++ variables.rb
@@ -0,0 +1,25 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+# VARIABLES -------------------------------------------------
+
+# variáveis são estruturas que guardam dados.
+my_name = "bianca"
+my_city = "sao joão de meriti"
+my_college = "ufrrj"
+my_food = "crepe"
+my_rabbit = "perninha"
+puts "my_name"
+puts my_name
+my_year = 1987+2
+
+
+
+# é possível concatenar estas variáveis com frases quaisquer
+puts "my name is #{my_name}, 
+I was born in #{my_city} in #{my_year}, 
+and my college is #{my_college}. 
+I had a rabbit called #{my_rabbit},
+Tonight I'll eat #{my_food}
+And that's It!!!!"
+
+
