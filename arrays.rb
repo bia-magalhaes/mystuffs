97dccf862a9935ebcb0672eca4775653731c5680
Bianca Magalhaes <biamagalhaes.tur@gmail.com>
2022-01-13 16:24:24 -0300

    2º dia

---
+++ arrays.rb
@@ -0,0 +1,13 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+my_array = [32, 38, 45]
+
+puts my_array
+puts "\nnossas idades são: #{my_array}"
+
+puts my_array[0]
+puts my_array[1]
+puts my_array[2]
+
+puts "a idade da Dannie é #{my_array[1]}"
+
---
+++ logicals.rb
@@ -0,0 +1,31 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+x = 6
+
+if x > 2
+    puts "legal, x é maior que 2"
+else 
+    puts "x não é > 2"
+end
+
+# ------------------------------------------------
+
+
+x = 6
+
+if x > 6
+    puts "legal, x é maior que 6"
+elsif x < 6
+    puts "x não é < 6"
+else
+    puts "não é nenhum dos dois"
+end
+# ---------------------------------------------
+
+horario = "dia"
+if horario == "noite"
+    puts "acendo a luz"
+else 
+    puts "apago a luz"
+end
+
