80c5381b89b2ce7dc5cd26148d7b088ea68c3bee
Bianca Magalhaes <biamagalhaes.tur@gmail.com>
2022-01-12 21:09:36 -0300

    Mais da 1ª aula

---
+++ operators.rb
@@ -0,0 +1,41 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+# OPERATORS ---------------------------------
+# Division (/)
+puts 11.0/2.0
+
+# Modulus (%)
+puts 11.0%2.0
+puts 17%7
+puts 23%3
+
+# COMPARISON OPERATORS
+puts 32 == 38
+puts 63 == 61+2
+puts "bianca" == "bianca"
+puts 32 != 38
+
+dannie_age = 38
+bia_age = 32
+
+puts "\ntesting age"
+puts dannie_age > bia_age
+puts dannie_age < bia_age
+
+# greater than or less than
+puts dannie_age <= 38
+puts bia_age >= dannie_age
+
+puts "\n"
+# range operators
+my_range = (1..10).to_a
+puts my_range
+
+puts "\n\n"
+
+my_range2 = (1...10).to_a
+puts my_range2
+
+
+
+
