bab4e66f23255181f8162f57c32f1f83287a1f2b
Bianca Magalhaes <biamagalhaes.tur@gmail.com>
2022-01-29 19:26:15 -0300

    About loops

--- logicals.rb
+++ logicals.rb
@@ -1,5 +1,6 @@
 #/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
 
+=begin
 x = 6
 
 if x > 2
@@ -7,10 +8,11 @@ if x > 2
 else 
     puts "x não é > 2"
 end
+=end
 
 # ------------------------------------------------
 
-
+=begin
 x = 6
 
 if x > 6
@@ -20,12 +22,29 @@ elsif x < 6
 else
     puts "não é nenhum dos dois"
 end
+=end
 # ---------------------------------------------
 
+=begin
 horario = "dia"
 if horario == "noite"
     puts "acendo a luz"
 else 
     puts "apago a luz"
 end
+=end
 
+# ----------------------------------------------
+my_name = "Nerly"
+
+if my_name == "bianca"
+    puts "meu nome é Bianca"
+elsif my_name == "dannie"
+    puts "irmã de bianca"
+elsif my_name == "Zé"
+    puts "cunhado de bianca"
+else 
+    puts "um estranho no ninho" 
+end
+
+# -----------------------------------------------
---
+++ logicals_loops.rb
@@ -0,0 +1,53 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+#LOOPS AND ITERATORS-------------------------
+#Iterator: percorre cada elemento de qualquer variável
+
+=begin
+age_of_family = [0.6, 33, 39, 46, 59, 60, 61, 62, 88]
+#puts age_of_family.last(3)
+#puts age_of_family.reverse
+
+
+age_of_family.reverse.each do |age| #>>>>> I can call 2 or + methods at the same time
+    puts "now I am printing element #{age}" #>>>>> this is iterator!!!!
+end
+=end
+
+# -------------------------------------------
+# About Loops
+=begin
+x = 0
+y = 10
+
+while x < y do 
+    puts "keep running because I am the number #{x}"
+    x += 1
+end
+=end
+# --------------------------------------------
+=begin
+x = 0
+
+while x <= 95 do 
+    puts "keep running because I am the number #{x}"
+    x += 5
+    puts "sum... now x = #{x}"
+end
+
+puts "I stopped running because now I am the number #{x}"
+=end
+
+#------------------------------------------------
+
+for e in 0..15
+    puts "o valor agora é #{e}"
+end
+
+(0..15).each do |e|
+    puts "o valor agora é #{e}"
+end
+
+
+
+
