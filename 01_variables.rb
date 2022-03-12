6c926980873408bf6aae50ce30111f66dc11d58f
Bianca Magalhaes <biamagalhaes.tur@gmail.com>
2022-03-12 20:48:06 -0300

    Reviewing class no 1 and 2
    That's it

---
+++ 01_variables.rb
@@ -0,0 +1,81 @@
+#/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
+
+# VARIABLES -------------------------------------------------
+
+# variáveis são estruturas que guardam dados.
+my_name = "bianca"
+my_city = "sao joão de meriti"
+my_college = "ufrrj"
+my_food = "crepe"
+my_rabbit = "perninha"
+puts "my_name"
+puts my_name
+my_year = 1987+2
+
+
+
+# é possível concatenar estas variáveis com frases quaisquer
+puts "my name is #{my_name}, 
+I was born in #{my_city} in #{my_year}, 
+and my college is #{my_college}. 
+I had a rabbit called #{my_rabbit},
+Tonight I'll eat #{my_food}
+And that's It!!!!"
+
+# ----------------------------------------------------
+# TIPOS DE VARIÁVEIS
+
+# STRING: Palavras, frases, etc. Sempre entre aspas
+# exemplo: "bianca"
+
+puts "\n\n"
+puts "exemplo de strings"
+string_01 = "batata frita"
+string_02 = "calabresa frita"
+
+puts "variáveis: #{string_01}, #{string_02}"
+puts "\n\n"
+
+# INTEGER: Números inteiros - idades, ano, etc. Sem aspas. 
+# exemplo: 1989, 45, -5 
+
+puts "exemplo de integer"
+inteiro_01 = 65
+inteiro_02 = -13
+
+puts "variáveis: #{inteiro_01}, #{inteiro_02}"
+puts "\n\n"
+
+# FLOAT: Números reais decimais.
+# exemplo: 1.2, 3.47, -5.4
+# OBS: Ruby obedece a anotação americana, por isso se usa ponto (.), não vírgula (,)
+puts "exemplo de float"
+decimal_01 = 2.5
+decimal_02 = -9.6
+
+puts "variáveis: #{decimal_01}, #{decimal_02}"
+puts "\n\n"
+
+# BOOLEAN: verdadeiro ou falso
+# exemplo: true, false
+# OBS: Quando eu não atribuo valor p/ ele, ele já é falso.
+puts "exemplo de boolean"
+verdade_sim = true
+mentira_nao = false
+
+puts "variáveis: #{verdade_sim}, #{mentira_nao}"
+puts "\n\n"
+
+# ARRAY: Conjuntos (numéricos, palavras, o que for)
+# exemplo: [32, 38, 45] << sempre usando colchetes
+# OBS: todo índice de array começa com 0
+
+
+
+#
+#
+
+#
+#
+
+
--- 02_operators.rb
+++ 02_operators.rb
@@ -1,28 +1,57 @@
 #/usr/share/rvm/rubies/ruby-2.7.5/bin/ruby
 
 # OPERATORS ---------------------------------
+# Soma (+)
+puts "exemplos de soma"
+puts 13+8
+puts "\n"
+
+# Subtraçao (-)
+puts "exemplos de subtraçao"
+puts 63-8
+puts "\n"
+
+# Muliplicaçao
+puts "exemplos de multiplicaçao"
+puts 3*6
+puts "\n"
+
 # Division (/)
+puts "exemplos de divisão"
 puts 11.0/2.0
+puts "\n"
 
 # Modulus (%)
+puts "exemplos de modulus"
 puts 11.0%2.0
 puts 17%7
 puts 23%3
+puts "\n\n"
 
 # COMPARISON OPERATORS
+
+puts "exemplos de operadores de comparaçao"
+puts "\n"
+
+puts "igualdade (== ou !=)"
+
 puts 32 == 38
 puts 63 == 61+2
 puts "bianca" == "bianca"
 puts 32 != 38
+puts "\n"
 
+puts "maior que (>), menor que (<)"
 dannie_age = 38
 bia_age = 32
 
 puts "\ntesting age"
 puts dannie_age > bia_age
 puts dannie_age < bia_age
+puts "\n"
 
 # greater than or less than
+puts "maior ou igual que (>=), menor ou igual que (<=)"
 puts dannie_age <= 38
 puts bia_age >= dannie_age
 
